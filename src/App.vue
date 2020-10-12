9fc4b4ead3803e7513e2dab1646272e6bd0e3f86
hannanmiah <36930986+hannanmiah@users.noreply.github.com>
2020-10-12 23:39:53 +0600

    This is my first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup(){
+    const item = ref(3)
+
+    return {
+      item
+    }
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <h6>{{ numberOfUsers }} user is availble right now!</h6>
+
+  <ul v-for="user in users">
+    <li>
+      {{ user.name }} ({{ user.age }})
+      <button class="delete" @click="deleteUser">Delete</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { ref, computed, reactive, toRefs } from "vue";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  setup() {
+    const user = reactive({
+      users: [
+        {
+          name: "Hannan Miah",
+          age: 24,
+          email: "hannanhridoy@gmail.com",
+        },
+        {
+          name: "Mustakim Sharkar",
+          age: 22,
+          email: "mustakimsharkar@gmail.com",
+        },
+      ],
+
+      numberOfUsers: computed(() => user.users.length),
+
+      deleteUser() {
+        user.users.pop();
+      },
+    });
+
+    return toRefs(user);
+  },
+};
+</script>
+
+<style scoped>
+button {
+  background: blue;
+  color: white;
+  border: 3px solid #ddd;
+  padding: 5px;
+}
+
+.delete {
+  background: red;
+  border: 1px solid orange;
+}
+</style>
